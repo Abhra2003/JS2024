0843d71f29f48808fe04ae562fa120866a18f16f
Abhra2003 <willturner2912@gmail.com>
2024-08-27 17:17:23 +0000

    slight changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,7 @@ account_City = "Jaipur"
 // accountID = 2 
 /*
 use const and let for variable declaration in javascript
+use console.table([a1,a2,... an]) to print multiple variables in one command
 */
 accountEmail = "athyu2gmail.com"
 accountPassword = "57463"
