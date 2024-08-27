c800efe706edd04fc8e63909d1a66b6bd0e72a5e
Abhra2003 <willturner2912@gmail.com>
2024-08-27 17:14:52 +0000

    learned about the ways to declare variables in javascript today

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 35664
+let accountEmail ="abhra@2003gmail.com"
+var accountPassword ="1234678"
+account_City = "Jaipur"
+// accountID = 2 
+/*
+use const and let for variable declaration in javascript
+*/
+accountEmail = "athyu2gmail.com"
+accountPassword = "57463"
+account_City = "kolkata"
+let account;
+console.table([accountID,account_City,accountEmail,accountPassword,account]);
